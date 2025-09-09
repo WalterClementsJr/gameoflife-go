ceb80edeab54b33b7ebc9991184d7412b5d9e9b3
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2025-09-09 12:00:49 +0700

    feat: UI

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"fmt"
+
+	rg "github.com/gen2brain/raylib-go/raygui"
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 const (
 	SquareSize = 20
@@ -12,7 +17,8 @@ type Game struct {
 	screenW       int32
 	screenH       int32
 	fps           int32
-	liveCellCount uint
+	pause         bool
+	liveCellCount int
 	pixelSize     int32
 
 	grid [][]int32
@@ -23,7 +29,7 @@ func (game *Game) init() {
 	game.screenW = ScreenW
 	game.screenH = ScreenH
 	game.pixelSize = SquareSize
-	game.fps = 5
+	game.fps = 10
 
 	game.grid = make([][]int32, int(game.screenW/game.pixelSize))
 
@@ -35,19 +41,49 @@ func (game *Game) init() {
 	rl.SetTargetFPS(game.fps)
 }
 
-func draw(game Game) {
-	rl.ClearBackground(rl.Gray)
-	drawGrid(game)
+func draw(game *Game) {
+	rl.ClearBackground(rl.RayWhite)
+
+	drawGrid(*game)
+	drawUI(game)
+}
+
+func drawUI(game *Game) {
+	// draw pause button
+	pauseX, pauseY, pauseW, pauseH := 400, 500, 80, 50
+
+	pauseBtn := rl.Rectangle{X: float32(pauseX), Y: float32(pauseY), Width: float32(pauseW), Height: float32(pauseH)}
+
+	var pauseState string
+	fmt.Println("game pause:", game.pause)
+	if game.pause {
+		pauseState = "Start"
+	} else {
+		pauseState = "Pause"
+	}
+
+	pause := rg.Button(pauseBtn, pauseState)
+	if pause {
+		game.pause = !game.pause
+	}
+
+	rl.DrawText(
+		fmt.Sprintf("Live cells: %d", game.liveCellCount),
+		200, 80, 20, rl.Red,
+	)
 }
 
 func drawGrid(game Game) {
 	for i := int32(1); i <= game.screenW; i += game.pixelSize {
-		rl.DrawLine(i, 0, i, game.screenH, rl.Red)
-		rl.DrawLine(0, i, game.screenW, i, rl.Red)
+		rl.DrawLine(i, 0, i, game.screenH, rl.Black)
+		rl.DrawLine(0, i, game.screenW, i, rl.Black)
 	}
 }
 
 func update(game *Game) {
+	if game.pause {
+		return
+	}
 }
 
 func main() {
@@ -57,7 +93,7 @@ func main() {
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 
-		draw(g)
+		draw(&g)
 		update(&g)
 
 		rl.EndDrawing()
